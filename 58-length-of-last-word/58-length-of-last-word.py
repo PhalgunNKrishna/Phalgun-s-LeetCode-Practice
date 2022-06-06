9094050ad16c1535adace06bfce94371c4e1526e
Phalgun Krishna <pnkrishna@ucdavis.edu>
2022-06-06 15:32:23 -0700

    Time: 28 ms (94.31%), Space: 13.9 MB (78.72%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -4,6 +4,8 @@ class Solution:
         li = s.split(' ')
         print(li)
         
+        
+        ### STUDY REVERSED FOR LOOP RANGE
         for i in range(len(li) - 1, -1, -1):
             print(li[i])
             if len(li[i]):
