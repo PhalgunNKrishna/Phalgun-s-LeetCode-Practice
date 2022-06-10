26c0df2f3f51f1e19cae71583f3826a5bf46f075
Phalgun Krishna <pnkrishna@ucdavis.edu>
2022-06-09 19:03:18 -0700

    Time: 36 ms (92.98%), Space: 13.9 MB (77.94%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.py
@@ -0,0 +1,70 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    
+    # SOLVED BUT EXCEEDED THE TIME LIMIT
+    def arotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        rotationsLeft = k
+        newTail = head
+        currTail = head
+        
+        if head == None:
+            return head
+        
+        if head.next == None:
+            return head
+        
+        if head.next.next == None:
+            while rotationsLeft:
+                currTail = head.next
+                currTail.next = head
+                newTail.next = None
+                head = currTail
+                rotationsLeft -= 1
+                newTail = head
+                currTail = head
+        
+        while rotationsLeft:
+            while newTail.next.next:
+                newTail = newTail.next
+                currTail = newTail.next
+            currTail.next = head
+            newTail.next = None
+            head = currTail
+            newTail = head
+            rotationsLeft -= 1
+            
+        return head
+    
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    
+        if not head:
+            return None
+        if not head.next:
+            return head
+        
+        # close the ring and keep track of how many nodes in list. Close by making tail connect to head
+        currTail = head
+        nodeCount = 1
+        while currTail.next:
+            currTail = currTail.next
+            nodeCount += 1
+        currTail.next = head
+
+        # new tail should be n - k % n - 1
+        # new head should be n - k % n
+        newTail = head
+        for i in range(nodeCount - k % nodeCount - 1):
+            newTail = newTail.next
+        newHead = newTail.next
+        
+        # break the ring
+        newTail.next = None
+        
+        return newHead
