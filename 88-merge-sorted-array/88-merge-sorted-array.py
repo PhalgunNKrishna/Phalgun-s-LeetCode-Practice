deb10a8ccd080eb11d09eb3aa9b8760461a6f24a
Phalgun Krishna <pnkrishna@ucdavis.edu>
2022-06-28 21:23:58 -0700

    Time: 56 ms (51.31%), Space: 14 MB (37.54%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
 
     ### NAIVE APPROACH
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def amerge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -15,3 +15,19 @@ class Solution:
         print(nums1)
         
         nums1.sort()
+        
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+        nums1copy = nums1[:m]
+        
+        p1 = 0
+        p2 = 0
+        
+        for p in range(n + m):
+            
+            if p2 >= n or (p1 < m and nums1copy[p1] < nums2[p2]):
+                nums1[p] = nums1copy[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
